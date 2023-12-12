be957d440eb02c3a8e0b53b4edf69a5ec55ab795
이호진 <gommind@naver.com>
2023-12-12 16:43:14 +0900

    Feat #3 hexagonal for order product save coupled test

--- order-service/src/main/java/com/example/orderservice/u_hexagonal/adapter/out/kafka/dto/KafkaOrderProductSaveRequestDTO.java
+++ order-service/src/main/java/com/example/orderservice/u_hexagonal/adapter/out/kafka/dto/KafkaOrderProductSaveRequestDTO.java
@@ -1,8 +1,10 @@
 package com.example.orderservice.u_hexagonal.adapter.out.kafka.dto;
 
 import lombok.Getter;
+import lombok.Setter;
 
 @Getter
+@Setter
 public class KafkaOrderProductSaveRequestDTO {
 
     private Long productNo;
